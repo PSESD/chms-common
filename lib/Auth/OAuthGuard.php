f3272112d6ad0cf836715a03b1b0bebc944c54be
Jake Oehler Morrison <jmorrison@psesd.org>
2016-03-10 18:33:32 -0800

    with oauth guard

---
+++ lib/Auth/OAuthGuard.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Clock Hour Management System
+ *
+ * @copyright Copyright (c) 2016 Puget Sound Educational Service District
+ * @license   MIT
+ */
+namespace CHMS\Common\Auth;
+
+use Authorizer;
+use Illuminate\Contracts\Auth\UserProvider;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\Auth\Guard as GaurdContract;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\GuardHelpers;
+
+class OAuthGuard implements GaurdContract
+{
+    use GuardHelpers;
+
+    /**
+     * @var string
+     */
+    protected $id;
+    /**
+     * @var Request
+     */
+    protected $request;
+    /**
+     * @var array
+     */
+    protected $config;
+
+    /**
+     * Constructor
+     *
+     * @param UserProvider $provider
+     * @param Request      $request
+     */
+    public function __construct($id, UserProvider $provider, Request $request, array $config = [])
+    {
+        $this->request = $request;
+        $this->provider = $provider;
+        $this->config = $config;
+        $this->id = $id;
+    }
+
+    /**
+     * Get the currently authenticated user.
+     *
+     * @return \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public function user()
+    {
+        if (!is_null($this->user)) {
+            return $this->user;
+        }
+        $user = null;
+        $resourceId = Authorizer::getResourceOwnerId();
+        $resourceType = Authorizer::getResourceOwnerType();
+        if ($resourceType !== $this->id) {
+            return false;
+        }
+        if (!empty($resourceId)) {
+            $user = $this->getProvider()->retrieveById($resourceId);
+        }
+        return $this->user = $user;
+
+    }
+
+    /**
+     * Validate a user's credentials.
+     *
+     * @param  array  $credentials
+     * @return bool
+     */
+    public function validate(array $credentials = [])
+    {
+        return false;
+    }
+
+    /**
+     * Gets the provider
+     *
+     * @return UserProvider
+     */
+    public function getProvider()
+    {
+        return $this->provider;
+    }
+}
