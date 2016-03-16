509323565a8d4759389f4d9076d6a965bc57a608
Jake Oehler Morrison <jmorrison@psesd.org>
2016-03-16 15:50:08 -0700

    bad tests [skip ci]

---
+++ tests/Http/Input/Validators/BaseValidatorTest.php
@@ -0,0 +1,63 @@
+<?php
+namespace CHMSTests\Hub\Http\Input\Validators;
+
+use CHMSTests\Hub\TestCase;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use CHMS\Hub\Models\User as UserModel;
+
+class BaseValidatorTest extends TestCase
+{
+    use DatabaseMigrations;
+    public function testSimpleValidate()
+    {
+        $model = new UserModel();
+        $faker = \Faker\Factory::create();
+        $input = [
+            'first_name' => $faker->firstName,
+            'last_name' => $faker->lastName,
+            'email' => $faker->email,
+            'password' => $faker->password
+        ];
+        $validator = $this->getValidator();
+        $this->assertTrue($validator->validate($model, $input, 'create'));
+    }
+
+    public function testSimpleValidateFail()
+    {
+        $model = new UserModel();
+        $faker = \Faker\Factory::create();
+        $input = [
+            'first_name' => $faker->firstName,
+            'last_name' => $faker->lastName,
+            'email' => $faker->email .'@coolguy.com',
+            'password' => $faker->password
+        ];
+        $validator = $this->getValidator();
+        $this->assertFalse($validator->validate($model, $input, 'create', false));
+        $this->assertArrayHasKey('email', $validator->errors);
+    }
+
+    /**
+     * @expectedException CHMS\Common\Exceptions\UnprocessableEntityHttpException
+     */
+    public function testSimpleValidateFailException()
+    {
+        $model = new UserModel();
+        $faker = \Faker\Factory::create();
+        $input = [
+            'first_name' => $faker->firstName,
+            'last_name' => $faker->lastName,
+            'email' => $faker->email .'@coolguy.com',
+            'password' => $faker->password
+        ];
+        $validator = $this->getValidator();
+        $this->assertFalse($validator->validate($model, $input, 'create'));
+        $this->assertArrayHasKey('email', $validator->errors);
+    }
+
+    public function getValidator()
+    {
+        $v = \Mockery::mock(\CHMS\Hub\Http\Input\Validators\BaseValidator::class);
+        return $v;
+    }
+}
---
+++ tests/Http/Transformers/BaseTransformerTest.php
@@ -0,0 +1,57 @@
+<?php
+namespace CHMSTests\Hub\Http\Transformers;
+
+use CHMSTests\Hub\TestCase;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use CHMS\Hub\Http\Transformers\BaseTransformer;
+use CHMS\Common\Contracts\Acl as AclContract;
+use CHMS\Hub\Models\User as UserModel;
+
+class BaseTransformerTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function testHubAdminTransform()
+    {
+        $s = microtime(true);
+        $model = $this->getUser();
+        $acl = $this->app->make(AclContract::class);
+        $acl->switchRoleContext(['hub_administrator']);
+        $f = $this->getTransformer();
+        $result = $f->transform($model);
+        $this->assertArrayHasKey('last_login', $result);
+        $this->assertArrayHasKey('ssn', $result);
+        $this->assertArrayHasKey('birth_date', $result);
+        $this->assertArrayNotHasKey('deleted_by', $result);
+    }
+
+
+    public function testStudentTransform()
+    {
+        $model = $this->getUser();
+        $acl = $this->app->make(AclContract::class);
+        $acl->switchRoleContext(['student']);
+        $f = $this->getTransformer();
+        $result = $f->transform($model);
+        $this->assertArrayHasKey('id', $result);
+        $this->assertArrayNotHasKey('first_name', $result);
+    }
+
+    public function testInstructorTransform()
+    {
+        $model = $this->getUser();
+        $acl = $this->app->make(AclContract::class);
+        $acl->switchRoleContext(['instructor']);
+        $f = $this->getTransformer();
+        $result = $f->transform($model);
+        $this->assertArrayHasKey('id', $result);
+        $this->assertArrayNotHasKey('last_login', $result);
+        $this->assertArrayNotHasKey('ssn', $result);
+    }
+
+
+    public function getTransformer()
+    {
+        return new \CHMS\Hub\Http\Transformers\User;
+    }
+}
